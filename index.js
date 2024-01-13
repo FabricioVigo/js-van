2170363555fd5f11233537841c2aaaa116ced0eb
fabricio <vigofabricio@gmail.com>
2024-01-13 18:56:54 -0300

    index html css y js

---
+++ index.js
@@ -0,0 +1,142 @@
+
+let data = [
+    {
+        "id": 1,
+        "name": "Lenovo Thinkpad",
+        "description": "Ryzen 7 8gb Ram",
+        "price": 450,
+        "img": "./assets/lenovo.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "Parlante Kassel",
+        "description": "PS082 Bluetooh",
+        "price": 110,
+        "img": "./assets/parlante.jpeg"
+    },
+    {
+        "id": 3,
+        "name": "Teclado Gamer",
+        "description": "Redragon K530W-RGB-PRO",
+        "price": 85,
+        "img": "./assets/teclado.jpeg"
+    },
+    {
+        "id": 4,
+        "name": "Disco Solido Kingston",
+        "description": "240Gb Kingston A400",
+        "price": 180,
+        "img": "./assets/disco.jpeg"
+    },
+    {
+        "id": 5,
+        "name": "Monitor Gamer 49''",
+        "description": "HRD 144Hz 4Ms XG49VQ",
+        "price": 250,
+        "img": "./assets/monitor.jpeg"
+    },
+    {
+        "id": 6,
+        "name": "Mouse Gamer",
+        "description": "M808W-KSA Storm Pro",
+        "price": 45,
+        "img": "./assets/mouse.jpeg"
+    },
+    {
+        "id": 7,
+        "name": "Jostick PC",
+        "description": "Redragon G807 Saturno",
+        "price": 90,
+        "img": "./assets/jostick.jpeg"
+    },
+    {
+        "id": 8,
+        "name": "Placa de Video",
+        "description": "RTX 4090 24Gb Msi Suprim X",
+        "price": 270,
+        "img": "./assets/placa.jpeg"
+    },
+    {
+        "id": 9,
+        "name": "Memoria Ram",
+        "description": "8Gb 3600 Mhz",
+        "price": 160,
+        "img": "./assets/ram.jpeg"
+    },
+    {
+        "id": 10,
+        "name": "Teclado Gamer Logitech",
+        "description": "G Pro LOL2 Mecanico",
+        "price": 60,
+        "img": "./assets/teclado2.jpeg"
+    },
+    {
+        "id": 11,
+        "name": "Notebook Samsung",
+        "description": "Pro Core i5 512Gb 16Gb",
+        "price": 650,
+        "img": "./assets/samsung.jpeg"
+    },
+    {
+        "id": 12,
+        "name": "Notebook Dell",
+        "description": "Pro i7 32Gb Ssd 512Gb 4Gb 16",
+        "price": 750,
+        "img": "./assets/dell.jpeg"
+    }
+]
+
+let productos = document.getElementById("producto")
+
+
+
+productos.innerHTML = ""
+
+data.forEach(pdt => {
+    console.log(pdt)
+    let divProductos = document.createElement("div")
+    divProductos.innerHTML = `
+        <div class="pdts">
+            <img class="imagen" src=${pdt.img}>
+                <h3 class="titulo">${pdt.name}</h3>
+                <span class="descripcion">${pdt.description}</span>
+                <p class="precio">${pdt.price}</p>
+            </div>
+`
+
+    productos.appendChild(divProductos)
+
+});
+
+
+let buscador = document.getElementById("buscador")
+
+buscador.addEventListener("keydown", (evento) => {
+    if (evento.key === 'Enter')
+        filtroBusqueda()
+})
+
+
+function filtroBusqueda() {
+    let productoBuscado = buscador.value.toLowerCase()
+
+    let nuevaBusqueda = data.filter((pdt) =>
+        pdt.name.toLowerCase().includes(productoBuscado)
+    )
+
+    productos.innerHTML = ""
+    nuevaBusqueda.forEach(pdt => {
+        console.log(pdt)
+        let divProductos = document.createElement("div")
+        divProductos.innerHTML = `
+            <div class="pdts">
+                <img class="imagen" src=${pdt.img}>
+                    <h3 class="titulo">${pdt.name}</h3>
+                    <span class="descripcion">${pdt.description}</span>
+                    <p class="precio">${pdt.price}</p>
+                </div>
+ 
+            `
+            productos.append(divProductos)
+    })
+}
